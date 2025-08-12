a91fdfaa201e7c0c688a27376507c0cef1a3db94
turbocat009 <76442361+turbocat009@users.noreply.github.com>
2025-08-12 17:47:38 +0200

    remade background in SDL2

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ main: src/main.c
 	gcc -O2 -Wall -o bin/main src/main.c -I/opt/X11/include -L/opt/X11/lib -lX11
 
 background: src/background.c
-	gcc -o bin/background src/background.c -I/opt/X11/include -L/opt/X11/lib -lX11 -lpng
+	gcc -o bin/background src/background.c -I/opt/homebrew/include/SDL2 -L/opt/homebrew/lib -lSDL2 -lSDL2_image
 
 clean:
 	rm -f main background
--- src/background.c
+++ src/background.c
@@ -1,43 +1,87 @@
-#include <X11/Xlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    Display *display;
-    Window window;
-    GC gc;
-    XEvent event;
-    int screen;
-
-    display = XOpenDisplay(NULL);
-    if (display == NULL)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        fprintf(stderr, "Cannot open display\n");
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
+    {
+        printf("SDL_image could not initialize! IMG_Error: %s\n", IMG_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_DisplayMode displayMode;
+    if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0)
+    {
+        printf("SDL_GetCurrentDisplayMode Error: %s\n", SDL_GetError());
+        SDL_Quit();
         return 1;
     }
 
-    screen = DefaultScreen(display);
-
-    window = XCreateSimpleWindow(display, RootWindow(display, screen),
-                                 10, 10, 640, 480, 1,
-                                 BlackPixel(display, screen), WhitePixel(display, screen));
-
-    XStoreName(display, window, "Background");
-    XMapWindow(display, window);
-
-    gc = XCreateGC(display, window, 0, NULL);
-    XSetForeground(display, gc, BlackPixel(display, screen));
-
-    while (1)
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window with Background",
+                                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          displayMode.w, displayMode.h, SDL_WINDOW_SHOWN);
+    if (!window)
     {
-        XNextEvent(display, &event);
-        XDrawString(display, window, gc, 50, 30, "Hello World", 11);
-
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        IMG_Quit();
+        SDL_Quit();
+        return -1;
     }
 
-    XFreeGC(display, gc);
-    XCloseDisplay(display);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Texture *backgroundTexture = IMG_LoadTexture(renderer, "image.png");
+    if (!backgroundTexture)
+    {
+        printf("Unable to load background image! IMG_Error: %s\n", IMG_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Event e;
+    int quit = 0;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = 1;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(backgroundTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
 
     return 0;
 }
